6d1a1e25e9edc8a0a5c47f9310bdc3871146ff2d
lmt666 <714046181@qq.com>
2018-12-08 15:06:13 +0800

    delete www

--- www/admin/inc/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-	$db = new PDO("mysql:host=127.0.0.1;dbname=blog;","root","root");
-	$db->query("SET NAMES 'utf8'");
-	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-}catch (PDOException $e){
-	echo $e->getMessage() . '<br/>';
-}
-?>
--- www/sessions/destroy.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  require_once '../inc/session.php';
-  logout();
-  redirect_to('../');
-?>
